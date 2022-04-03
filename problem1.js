391b7e53cccbe7077e69ee0a6a52b097d0fb3c69
Mike <shpoopdy@gmail.com>
2022-04-03 14:58:05 -0700

    Add regex check for numbers in string

--- problem1.js
+++ problem1.js
@@ -1,21 +1,44 @@
 function wordThing() {
   let isFive = false;
-  let hasNumber = false;
+  let noNumber = true;
+  let givenWord = "";
+  let goodWord = false;
 
-  while(!isFive || !hasNumber) {
-    let givenWord = window.prompt("Enter a five letter word: ");
-    for(let i = 0; i < givenWord.length; i++) {
-      if(givenWord[i].isNaN()) {
-        hasNumber = true;
-        break;
-      }
+  while(!goodWord) {
+    givenWord = window.prompt("Enter a five letter word: ");
+    hasNumber = /\d/;
+    if(hasNumber.test(givenWord)) {
+      noNumber = false;
     }
 
-    if(givenWord.length < 5 || givenWord.length > 5) {
-      window.prompt("Five letter words only!");
-      break;
-    }
-    else {
+    if(givenWord.length == 5) {
       isFive = true;
     }
+
+    if(isFive && noNumber) {
+      goodWord = true;
+    }
+  }
 }
+
+window.addEventListener("load", wordThing, false);
+
+/*
+Gets all combinations. I should make a check to make sure that the returned
+words are of length 3.
+
+var tree = function(leafs) {
+  var branches = [];
+  if (leafs.length == 1) return leafs;
+  for (var k in leafs) {
+    var leaf = leafs[k];
+    tree(leafs.join('').replace(leaf, '').split('')).concat("").map(function(subtree) {
+      branches.push([leaf].concat(subtree));
+    });
+  }
+  return branches;
+};
+console.log(tree("abc".split('')).map(function(str) {
+  return str.join('')
+}))
+*/
